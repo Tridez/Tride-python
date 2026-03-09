ed31edfa0b28211a4beeda548c0b06d2cb01ebe9
Tridez <y1692368558@gmail.com>
2026-03-09 10:06:26 +0800

    Add files via upload

---
+++ EP4.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.youtube.com/"
+
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text,"html.parser")
+titles = soup.find_all('a')
+
+
+print(response.status_code)
+
+for title in titles:
+    text = title.get_text().strip()
+    link = title.get("href")
+
+    if text and link:
+        print(f"Title:{text},Link:{link}")
+
+
